5ec63b37dd2a3398c3ec802dfd407826a16c5bca
Roni Laukkarinen <roni@dude.fi>
2022-01-11 17:13:17 +0200

    Fix typo in command

--- bin/tasks/acf.sh
+++ bin/tasks/acf.sh
@@ -2,10 +2,10 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:26:55
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2021-11-23 18:40:41
+# @Last Modified time: 2022-01-11 17:13:13
 
 # Let's ensure we have acf-json folder
 mkdir -p ${PROJECT_THEME_PATH}/acf-json
 
 # Ensure file permissions for that folder
-chmdo -R 777 ${PROJECT_THEME_PATH}/acf-json
+chmod -R 777 ${PROJECT_THEME_PATH}/acf-json
