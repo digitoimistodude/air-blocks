140903c8a7b62b884b8f8939402cfe85c5154395
Roni Laukkarinen <roni@dude.fi>
2022-01-11 17:21:11 +0200

    Fix permissions command

--- bin/tasks/acf.sh
+++ bin/tasks/acf.sh
@@ -2,10 +2,10 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:26:55
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-01-11 17:13:13
+# @Last Modified time: 2022-01-11 17:16:01
 
 # Let's ensure we have acf-json folder
 mkdir -p ${PROJECT_THEME_PATH}/acf-json
 
 # Ensure file permissions for that folder
-chmod -R 777 ${PROJECT_THEME_PATH}/acf-json
+sudo chmod -R 777 ${PROJECT_THEME_PATH}/acf-json
