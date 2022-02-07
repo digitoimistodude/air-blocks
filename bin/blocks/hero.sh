71f65fc385070d277a7a876b75b9edb11bac381e
Roni Laukkarinen <roni@dude.fi>
2022-02-07 17:22:29 +0200

    Fix function replaces

--- bin/blocks/hero.sh
+++ bin/blocks/hero.sh
@@ -2,7 +2,7 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:23:24
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-07 16:37:26
+# @Last Modified time: 2022-02-07 17:22:21
 
 # Block specific variables
 export BLOCK_ACF_JSON_FILE="${AIRBLOCKS_THEME_PATH}/acf-json/group_618bb10852874.json"
@@ -18,17 +18,21 @@ cp -nv ${AIRBLOCKS_THEME_PATH}/sass/components/_heading-hero.scss ${PROJECT_THEM
 # cp -nv ${AIRBLOCKS_THEME_PATH}/svg/play.svg ${PROJECT_THEME_PATH}/svg/
 
 # Register ACF block in functions.php
+# TODO: Add localization
 sed -e "/\'acf_blocks\' \=\> \[/a\\
       [|\
        'name' => 'hero',|\
        'title' => 'Sivun yläosa',|\
-      ],\\" < functions.php | tr '|' '\n' < ${AIRBLOCKS_THEME_PATH}/functions.php > ${AIRBLOCKS_THEME_PATH}/functions_with_changes.php
+      ],\\" < ${PROJECT_THEME_PATH}/functions.php | tr '|' '\n' < ${PROJECT_THEME_PATH}/functions.php > ${PROJECT_THEME_PATH}/functions_with_changes.php
 
 # Remove the file without any changes
-rm ${AIRBLOCKS_THEME_PATH}/functions.php
+rm ${PROJECT_THEME_PATH}/functions.php
 
 # Rename the changed file to the official one
-mv ${AIRBLOCKS_THEME_PATH}/functions_with_changes.php ${AIRBLOCKS_THEME_PATH}/functions.php
+mv ${PROJECT_THEME_PATH}/functions_with_changes.php ${PROJECT_THEME_PATH}/functions.php
+
+# Ask block name
+echo "${BOLDYELLOW}Importing ACF fields (json)...${TXTRESET} "
 
 # Check if translations are needed
 if [[ ${AIR_BLOCKS_LANG} = "en" ]]; then
