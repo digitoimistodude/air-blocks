f6857a0c6c5eee69a82f1f803cc45d630c000e24
Roni Laukkarinen <roni@dude.fi>
2022-02-08 12:55:38 +0200

    Fixes for localization process

--- bin/tasks/localization.sh
+++ bin/tasks/localization.sh
@@ -2,23 +2,18 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:07:42
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-08 10:50:02
+# @Last Modified time: 2022-02-08 12:55:13
 
 # Translate strings
-sed -i'' \
--e 's/\"title\"\: \"Lohko\: Sivun yläosa\"\,/\"title\"\: \"Block\: Hero\"\,/' \
--e 's/\"label\"\: \"Taustakuva\"\,/\"title\"\: \"Background image\"\,/' \
--e 's/\"label\"\: \"Taustavideo \(\.mp4\, max 2mb\)\"\,/\"label\"\: \"Background video \(\.mp4\, max 2mb\)\"\,/' \
--e 's/\"label\"\: \"Yläotsikko\"\,/\"label\"\: \"Upper title\"\,/' \
--e 's/\"instructions\"\: \"Jos kenttä on jätetty tyhjäksi\, tilalla näytetään murupolku\.\"\,/\"instructions\"\: \"If field is left out empty\, will show breadcrumbs in place\.\"\,/' \
--e 's/\"label\"\: \"Otsikko\"\,/\"label\"\: \"Heading\"\,/' \
--e 's/\"label\"\: \"Tekstisisältö\"\,/\"label\"\: \"Text content\"\,/' \
--e 's/\"label\"\: \"Nappi\"\,/\"label\"\: \"Button\"\,/' \
--e 's/\"label\"\: \"Tyyli\"\,/\"label\"\: \"Style\"\,/' ${BLOCK_ACF_JSON_PATH} > ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json;
+sed -i'' -e 's/\"title\"\: \"Lohko\: Sivun yläosa\"\,/\"title\"\: \"Block\: Hero\"\,/
+s/\"label\"\: \"Taustakuva\"\,/\"title\"\: \"Background image\"\,/
+s/\"label\"\: \"Taustavideo/\"label\"\: \"Background video/
+s/\"label\"\: \"Yläotsikko\"\,/\"label\"\: \"Upper title\"\,/
+s/\"instructions\"\: \"Jos kenttä on jätetty tyhjäksi\, tilalla näytetään murupolku\.\"\,/\"instructions\"\: \"If field is left out empty\, will show breadcrumbs in place\.\"\,/
+s/\"label\"\: \"Otsikko\"\,/\"label\"\: \"Heading\"\,/
+s/\"label\"\: \"Tekstisisältö\"\,/\"label\"\: \"Text content\"\,/
+s/\"label\"\: \"Nappi\"\,/\"label\"\: \"Button\"\,/
+s/\"label\"\: \"Tyyli\"\,/\"label\"\: \"Style\"\,/' ${BLOCK_ACF_JSON_PATH} > ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json;
 
 # Import the localized file with ACF fields
-cp -nv ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json ${PROJECT_THEME_PATH}/acf-json/
-mv ${PROJECT_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json ${PROJECT_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}
-
-# Remove the leftover file
-rm ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json
+cp -nv ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json ${PROJECT_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}
