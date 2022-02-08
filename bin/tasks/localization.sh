f88766e5295e1285e244fe2983da9b09e809bb2e
Roni Laukkarinen <roni@dude.fi>
2022-02-08 10:50:10 +0200

    Fix mistake in sed multi-line command

--- bin/tasks/localization.sh
+++ bin/tasks/localization.sh
@@ -2,18 +2,18 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:07:42
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-08 10:47:29
+# @Last Modified time: 2022-02-08 10:50:02
 
 # Translate strings
 sed -i'' \
--e 's/\"title\"\: \"Lohko\: Sivun yläosa\"\,/\"title\"\: \"Block\: Hero\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Taustakuva\"\,/\"title\"\: \"Background image\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Taustavideo \(\.mp4\, max 2mb\)\"\,/\"label\"\: \"Background video \(\.mp4\, max 2mb\)\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Yläotsikko\"\,/\"label\"\: \"Upper title\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"instructions\"\: \"Jos kenttä on jätetty tyhjäksi\, tilalla näytetään murupolku\.\"\,/\"instructions\"\: \"If field is left out empty\, will show breadcrumbs in place\.\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Otsikko\"\,/\"label\"\: \"Heading\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Tekstisisältö\"\,/\"label\"\: \"Text content\"\,/' ${BLOCK_ACF_JSON_PATH} \
--e 's/\"label\"\: \"Nappi\"\,/\"label\"\: \"Button\"\,/' ${BLOCK_ACF_JSON_PATH} \
+-e 's/\"title\"\: \"Lohko\: Sivun yläosa\"\,/\"title\"\: \"Block\: Hero\"\,/' \
+-e 's/\"label\"\: \"Taustakuva\"\,/\"title\"\: \"Background image\"\,/' \
+-e 's/\"label\"\: \"Taustavideo \(\.mp4\, max 2mb\)\"\,/\"label\"\: \"Background video \(\.mp4\, max 2mb\)\"\,/' \
+-e 's/\"label\"\: \"Yläotsikko\"\,/\"label\"\: \"Upper title\"\,/' \
+-e 's/\"instructions\"\: \"Jos kenttä on jätetty tyhjäksi\, tilalla näytetään murupolku\.\"\,/\"instructions\"\: \"If field is left out empty\, will show breadcrumbs in place\.\"\,/' \
+-e 's/\"label\"\: \"Otsikko\"\,/\"label\"\: \"Heading\"\,/' \
+-e 's/\"label\"\: \"Tekstisisältö\"\,/\"label\"\: \"Text content\"\,/' \
+-e 's/\"label\"\: \"Nappi\"\,/\"label\"\: \"Button\"\,/' \
 -e 's/\"label\"\: \"Tyyli\"\,/\"label\"\: \"Style\"\,/' ${BLOCK_ACF_JSON_PATH} > ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json;
 
 # Import the localized file with ACF fields
