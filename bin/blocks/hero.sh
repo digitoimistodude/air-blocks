9f942527e68466b063e0fda96c2dc04157ca62f0
Roni Laukkarinen <roni@dude.fi>
2022-02-08 15:53:10 +0200

    Final improvements to block script

--- bin/blocks/hero.sh
+++ bin/blocks/hero.sh
@@ -2,7 +2,7 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:23:24
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-08 15:19:28
+# @Last Modified time: 2022-02-08 15:45:42
 
 # Block specific variables
 export BLOCK_ACF_JSON_FILE="group_618bb10852874.json"
@@ -19,15 +19,9 @@ cp -nv ${AIRBLOCKS_THEME_PATH}/sass/components/_heading-hero.scss ${PROJECT_THEM
 # cp -nv ${AIRBLOCKS_THEME_PATH}/svg/play.svg ${PROJECT_THEME_PATH}/svg/
 
 # Register ACF block in functions.php
-# TODO: Add localization
+# Please note: The title of the block will be translated in localization.sh if en is selected
 sed -e "/\'acf_blocks\' \=\> \[/a\\
       [|\
        'name' => 'hero',|\
        'title' => 'Sivun yläosa',|\
-      ],\\" < ${PROJECT_THEME_PATH}/functions.php | tr '|' '\n' < ${PROJECT_THEME_PATH}/functions.php > ${PROJECT_THEME_PATH}/functions_with_changes.php
-
-# Remove the file without any changes
-rm ${PROJECT_THEME_PATH}/functions.php
-
-# Rename the changed file to the official one
-mv ${PROJECT_THEME_PATH}/functions_with_changes.php ${PROJECT_THEME_PATH}/functions.php
+      ],\\" < ${PROJECT_THEME_PATH}/functions.php | tr '|' '\n' > ${PROJECT_THEME_PATH}/tmpfile
--- bin/tasks/localization.sh
+++ bin/tasks/localization.sh
@@ -2,9 +2,9 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:07:42
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-08 15:17:48
+# @Last Modified time: 2022-02-08 15:51:37
 
-# Translate strings
+# Translate strings for block json (defined in the sh file of the selected block)
 sed -i'' -e 's/\"title\"\: \"Lohko\: Sivun yläosa\"\,/\"title\"\: \"Block\: Hero\"\,/
 s/\"label\"\: \"Taustakuva\"\,/\"title\"\: \"Background image\"\,/
 s/\"label\"\: \"Taustavideo/\"label\"\: \"Background video/
@@ -15,3 +15,5 @@ s/\"label\"\: \"Tekstisisältö\"\,/\"label\"\: \"Text content\"\,/
 s/\"label\"\: \"Nappi\"\,/\"label\"\: \"Button\"\,/
 s/\"label\"\: \"Tyyli\"\,/\"label\"\: \"Style\"\,/' ${BLOCK_ACF_JSON_PATH}
 
+# Translate blocks for functions.php, make changes directly to the file
+sed -e 's/Sivun yläosa/Hero/' ${PROJECT_THEME_PATH}/functions.php > ${PROJECT_THEME_PATH}/tmpfile
