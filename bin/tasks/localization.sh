23e301f0e8d0bf35d6a0861e16569d39c40b2be4
Roni Laukkarinen <roni@dude.fi>
2022-02-08 10:36:11 +0200

    Fix json path

--- bin/tasks/localization.sh
+++ bin/tasks/localization.sh
@@ -2,7 +2,7 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-11-23 18:07:42
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-08 10:29:56
+# @Last Modified time: 2022-02-08 10:35:30
 
 # Localization variables
 LOCALIZED_STRINGS="${SCRIPTS_LOCATION}/tasks/localization.properties"
@@ -10,7 +10,7 @@ LOCALIZED_STRINGS="${SCRIPTS_LOCATION}/tasks/localization.properties"
 # Loop through strings and update file
 while IFS="=" read -r TRANSLATE_FROM TRANSLATE_TO;
 do
-  sed -i'' -e 's/$TRANSLATE_FROM/$TRANSLATE_TO/' ${BLOCK_ACF_JSON_FILE}
+  sed -i'' -e "s/$TRANSLATE_FROM/$TRANSLATE_TO/" ${BLOCK_ACF_JSON_PATH}
 done < "${LOCALIZED_STRINGS}" > ${AIRBLOCKS_THEME_PATH}/acf-json/${BLOCK_ACF_JSON_FILE}_en.json;
 
 # Import the localized file with ACF fields
