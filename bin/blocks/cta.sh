93a0cc35ce71eb6fa5cba00595a8228743d1c074
Roni Laukkarinen <roni@dude.fi>
2022-02-10 16:11:03 +0200

    Clean ups

--- bin/blocks/cta.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# @Author: Roni Laukkarinen
-# @Date:   2021-11-23 18:23:24
-# @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2021-11-23 18:39:40
-echo 'This is a cta block';
