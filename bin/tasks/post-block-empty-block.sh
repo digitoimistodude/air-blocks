c2e6d592045d6cac280daf336215e5c413271381
Roni Laukkarinen <roni@dude.fi>
2022-02-23 14:25:37 +0200

    Translate word Block

--- bin/tasks/post-block-empty-block.sh
+++ bin/tasks/post-block-empty-block.sh
@@ -2,7 +2,7 @@
 # @Author: Roni Laukkarinen
 # @Date:   2022-02-08 16:03:52
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2022-02-22 13:44:35
+# @Last Modified time: 2022-02-23 14:25:20
 # Tasks after functions.php has been updated
 
 # Let's go to the project directory
@@ -23,3 +23,10 @@ cp -nv ${BLOCK_ACF_JSON_PATH} ${PROJECT_THEME_PATH}/acf-json/
 # Activate block ACF fields
 echo "${BOLDYELLOW}Activating ACF fields for the block...${TXTRESET} "
 vendor/wp-cli/wp-cli/bin/wp eval "acf_import_field_group( json_decode( file_get_contents( '$BLOCK_ACF_JSON_PATH' ), true ) );"
+
+# Check if translations are needed
+if [[ ${AIR_BLOCKS_LANG} = "en" ]]; then
+
+  # General ACF fields, make changes directly to the file
+  sed -i'' -e 's/Lohko\:/Block\:/' ${BLOCK_ACF_JSON_PATH}
+fi
