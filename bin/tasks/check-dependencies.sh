16cf37bbf392b33e676de6a6c93e463ca740bac4
Roni Laukkarinen <roni@dude.fi>
2022-12-05 19:07:27 +0200

    Make block check more reliable

--- bin/tasks/check-dependencies.sh
+++ bin/tasks/check-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if block is registered with the same name
-if grep -q ${BLOCK_NAME} ${PROJECT_THEME_PATH}/functions.php; then
+if grep -q \'"$BLOCK_NAME"\' ${PROJECT_THEME_PATH}/functions.php; then
   echo "
 ${RED}Found exactly the same block name registered in functions.php... The script will now quit.${TXTRESET}
 " 1>&2
