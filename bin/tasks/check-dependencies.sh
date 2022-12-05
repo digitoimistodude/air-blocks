463752db78efbe7eaa136dcceb52f16712c83f0d
Roni Laukkarinen <roni@dude.fi>
2022-12-05 17:47:48 +0200

    Line breaks

--- bin/tasks/check-dependencies.sh
+++ bin/tasks/check-dependencies.sh
@@ -2,7 +2,9 @@
 
 # Check if block is registered with the same name
 if grep -q ${BLOCK_NAME} ${PROJECT_THEME_PATH}/functions.php; then
-  echo "${RED}Found exactly the same block name registered in functions.php... The script will now quit.${TXTRESET}" 1>&2
+  echo "
+${RED}Found exactly the same block name registered in functions.php... The script will now quit.${TXTRESET}
+" 1>&2
   exit
 else
   # If not found, continue
